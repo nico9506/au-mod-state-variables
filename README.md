1d156e8d7523aabd3186838ae32d4dd1627e7a60
Nicolas Lopez <niclopezt@gmail.com>
2025-07-17 14:19:09 +0700

    deploy contract on SEPOLIA

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # au-mod-state-variables
-Hardhat practice: Modifiying state variables
+
+Hardhat practice: Modifying state variables
+
+### Contract deployed in SEPOLIA
+
+Address: 0xdd616F43e73271F8Eb8e9cAd9Ee0007F4e5D4019
---
+++ scripts/ModifyVariable.js
@@ -0,0 +1,32 @@
+const ethers = require("ethers");
+const hre = require("hardhat");
+require("dotenv").config();
+
+async function main() {
+  const URL = process.env.ALCHEMY_TESTNET_RPC_URL;
+
+  const artifacts = await hre.artifacts.readArtifact("ModifyVariable");
+
+  const provider = new ethers.providers.JsonRpcProvider(URL);
+
+  const TESTNET_PRIVATE_KEY = process.env.TESTNET_PRIVATE_KEY;
+
+  const wallet = new ethers.Wallet(TESTNET_PRIVATE_KEY, provider);
+
+  const factory = new ethers.ContractFactory(
+    artifacts.abi,
+    artifacts.bytecode,
+    wallet
+  );
+
+  // chose `100` as initial value to provide the constructor
+  const modifyVariable = await factory.deploy(100);
+  await modifyVariable.deployed();
+
+  console.log("ModifyVariable contract address:", modifyVariable.address);
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exitCode = 1;
+});
